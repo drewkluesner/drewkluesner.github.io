da183f9d83b55c6e7858cff15098197272387646
Drew Kluesner <122191953+drewkluesner@users.noreply.github.com>
2023-02-24 18:59:08 -0600

    iacf

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2022ia",
+      "defaultValue": "2022iacf",
       "required": "true",
       "disabled": "true"
     },
